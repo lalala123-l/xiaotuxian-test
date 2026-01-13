6bc6def97c834c30994eb3242f3e8e53fecd8715
lalala123-l <2871967604@qq.com>
2026-01-13 10:16:31 +0800

    feat: 首页轮播图

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,28 @@
-<script setup lang="ts">
-//
-</script>
-
 <template>
-  <view class="index">index</view>
+  <view class="index">
+    <custom-navbar></custom-navbar>
+    <Swiper :list="bannerList"></Swiper>
+  </view>
 </template>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import CustomNavbar from './components/CustomNavbar.vue'
+import Swiper from './components/Swiper.vue'
 
+import type { BannerItem } from '@/types/home'
+import { getHomeBannerAPI } from '@/api/home'
+import { onLoad } from '@dcloudio/uni-app'
+
+// 获取轮播图数据
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+onLoad(() => {
+  getHomeBannerData()
+})
+</script>
 <style lang="scss">
 //
 </style>
