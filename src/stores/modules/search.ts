cd9a8984212f5b02713005931c9394f01d6e6754
lalala123-l <2871967604@qq.com>
2026-01-19 15:49:40 +0800

    feat: 完善搜索组件

--- src/stores/modules/search.ts
+++ src/stores/modules/search.ts
@@ -1,24 +1,33 @@
 import { defineStore } from 'pinia'
 import { ref, watch } from 'vue'
 
+const SEARCH_HISTORY_KEY = 'searchHistory'
+const MAX_HISTORY_LENGTH = 20
+
 export const useHistoryStore = defineStore(
   'searchHistoryStore',
   () => {
-    //历史列表信息
-    const historyList = ref<string[]>(['毛衣', '手套', '口红'])
-    // 2. 初始化：从本地缓存加载历史记录
+    const historyList = ref<string[]>([])
     const initHistory = () => {
-      const historyStr = uni.getStorageSync('searchHistory') || '[]'
-      historyList.value = JSON.parse(historyStr)
+      try {
+        const historyStr = uni.getStorageSync('SEARCH_HISTORY_KEY')
+        historyList.value = JSON.parse(historyStr) || []
+      } catch (error) {
+        console.error('加载搜索历史失败：', error)
+        historyList.value = []
+        uni.setStorageSync(SEARCH_HISTORY_KEY, '[]')
+      }
     }
     //保存历史记录
     const addHistory = (val: string) => {
       if (historyList.value.find((v) => v == val)) {
         return
       }
-      historyList.value.push(val)
+      //加载到数组的头部
+      historyList.value.unshift(val)
     }
     const deleteHistory = (index: number) => {
+      if (index < 0 || index >= historyList.value.length) return
       historyList.value.splice(index, 1)
     }
     //清除历史信息
